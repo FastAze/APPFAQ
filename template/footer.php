91494fa856b0760177c0a2053cff09a791e5e131
FastAze <85764104+FastAze@users.noreply.github.com>
2025-02-11 17:10:34 +0000

    Fixer le footer en le rendant fixe en bas de page et mettre à jour le contenu du footer pour inclure les noms des contributeurs

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,8 @@
 <footer>
     <div class="footer-container">
-        <p>&copy; 2025 APP F.A.Q - Tous droits réservés</p>
+        <p>&copy; 2025 APP F.A.Q - HONTANS Sylvain, CHAILLOU Baptiste,  </p>
         <nav>
-            <a href="mentions-legales.php">Mentions légales</a>
             <a href="contact.php">Contact</a>
-            <a href="about.php">À propos</a>
         </nav>
     </div>
 </footer>
