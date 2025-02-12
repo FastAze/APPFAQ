575b1f9856c2b3996d8ae7388b16a6817fee91a9
Hontans <163842666+Hontans@users.noreply.github.com>
2025-02-12 21:54:59 +0000

    Refactor FAQ section layout for improved readability

--- pages/list.php
+++ pages/list.php
@@ -16,16 +16,16 @@
 </head>
 <body>
     <div class="mid">
-    <div class="faq">
-        <button class="question">FAQ</button>
-        <div class="reponse">
-        <p>Reponse</p>
-        <!-- Ajout des FAQ et de leurs réponses avec le PHP -->
-        <p>
-            <input class="reponse-btn"  type="button" id="repondre" name="repondre" value="Répondre"/> <!-- Avec ce bouton on pourra afficher la réponse à la question -->
-        </p>
+        <div class="faq">
+            <button class="question">FAQ</button>
+            <div class="reponse">
+            <p>Reponse</p>
+            <!-- Ajout des FAQ et de leurs réponses avec le PHP -->
+            <p>
+                <input class="reponse-btn"  type="button" id="repondre" name="repondre" value="Répondre"/> <!-- Avec ce bouton on pourra afficher la réponse à la question -->
+            </p>
+            </div>
         </div>
-    </div>
     </div>
 </body>
 </html>
