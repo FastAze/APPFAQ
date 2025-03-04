e9a855cf919eecfee66c73006fa0d092f480099b
Hontans <163842666+Hontans@users.noreply.github.com>
2025-03-04 17:57:55 +0000

    Suppression des fichiers CSS, JS et PHP liés à l'animation de nuages obsolète

--- template/php/cloud.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-        <div class="cloud">
-            <h2>Protefolio</h2>
-            <div class="drop" style="left: 37px; font-size: 0.8em; animation-duration: 1s;">@</div>
-            <div class="drop" style="left: 100px; font-size: 0.5em; animation-duration: 0.5s;">#</div>
-        </div>
-</div>
