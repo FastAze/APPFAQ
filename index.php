575b0976edaa8e56b4247b1999dc2ec201e10225
FastAze <85764104+FastAze@users.noreply.github.com>
2025-03-11 14:25:26 +0000

    Correction du chemin de la feuille de style et ajout d'un fichier de test dans la page de réinitialisation du mot de passe

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-    <link rel="stylesheet" href="<?= chemin ?>/pages/css/main.css">
+    <link rel="stylesheet" href="<?= $chemin ?>/pages/css/main.css">
 </head>
 <body>
     <h1 id="titre-faq">F.A.Q</h1>
