1f34efa6f876160f5f79ba59be3fbff7eda0e82f
FastAze <baptiste.chaillou@outlook.fr>
2025-03-15 03:49:33 +0100

    Ajout de la fonctionnalité d'ajout de questions avec gestion des erreurs et redirection, et désactivation du démarrage de session dans ini.php.

--- template/php/ini.php
+++ template/php/ini.php
@@ -26,7 +26,7 @@
             return $dbh;
         }
     }
-    session_start();
+    // session_start();
     // ini_set('display_errors', 1);
     // ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
